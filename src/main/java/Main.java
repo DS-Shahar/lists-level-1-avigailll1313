026553d4d8d7f55a57f3b7d93f732d0902c47ae1
avigailll1313 <avigacohen07@gmail.com>
2024-12-09 16:50:12 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -115,14 +115,99 @@ public static void q4(Node<Integer> head) {
     head= head.getNext();}}
   
     
+public static boolean q5(Node<Integer> head, int num) {
+	
+	int n= head.getValue();
+	if (n==num) {
+	return true;
+	}
+    head= head.getNext();
+    while(head !=null  ) {
+    	 n= head.getValue();
+    	if (n==num) {
+    return  true;}
+    head= head.getNext();}
+return false;}
+
+public static Node<Integer> q6 (Node<Integer> head, int num) {
+	
+	int n= head.getValue();
+	if (n==num) {
+		head.setNext(head.getNext().getNext());
+         return head.getNext();		}
+	
+	Node<Integer> current = head;
+    while(head !=null  ) {
+    	 n= head.getValue();
+    	 if (current.getNext().getValue() == num) {
+             // מסירים את החוליה
+             current.setNext(current.getNext().getNext());
+             return head;  // מחזירים את ראש הרשימה
+         }
+         current = current.getNext();  // נעים לחוליה הבאה 
+         }  return head;  	}
+
+
+
+	
+public static Node<Integer> q7 (Node<Integer> head, int num) {
+	if(num==1) {
+	        if (head != null) {
+	            return head.getNext();  // מחזירים את החוליה הבאה אחרי הראש
+	        }}
+	Node<Integer> current = head;
+	for (int i=1;i<num-1 ;i++){
+		current= current.getNext();}
+	
+    	int n= head.getValue();
+   
+             current.setNext(current.getNext().getNext());
+             return head;  // מחזירים את ראש הרשימה
+}
+
+public static boolean q8(Node<Integer> l1, Node<Integer> l2) {
+    if (l1 == null) {
+        return true;
+    }
+
+    if (l2 == null) {
+        return false;
+    }
+
+    if (l1.getValue() == l2.getValue()) {
+        return q8(l1.getNext(), l2);  // מחפשים את הערך הבא ב-L1 ב-L2
+    }
+
+    return q8(l1, l2.getNext());
+}
+    
+public static void q9(Node<Integer> l1, Node<Integer> l2) {
+boolean b;
+
+	if (l1 != null) {
+		while (l2 != null) {
+		b=q5(l1,l2.getValue());
+		if( b==true) {
+			System.out.println(l2.getValue());
+		}
+		l2= l2.getNext();
+	}
+}}
+
 
 
 public static void main(String[] args) {
 	    int	[] arr = {4,5,6,7,8,};
+	    int	[] arr2 = {4,7,8,};
 //	    	randomNumbers<Integer> numbers = generateRandomNumbers(1, 100);
 	        System.out.println(q1s1(arr));
 	    //    q2(q1s1(arr));
 	      //q3();
 	        q4(q1s1(arr));
+	        System.out.println(  q6(q1s1(arr),5));
+	        System.out.println(  q5(q1s1(arr),5));
+	        System.out.println(  q7(q1s1(arr),4));
+	        System.out.println(  q8(q1s1(arr),q1s1(arr2)));
+	         q9(q1s1(arr),q1s1(arr2));
 	    }
-	}
+}
